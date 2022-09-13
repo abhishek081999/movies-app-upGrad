3eb7a249f3491bca9153b284c2399deac04e082d
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-09-13 20:40:16 +0530

    Added Released Movie Content & Changed Cursor on Hover

---
+++ src/common/ReleasedMovieContent.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import './ReleasedMovieContent.css';
+
+const ReleasedMovieContent = ({ data }) => {
+
+    function releaseDate(date) {
+        let releaseDate = new Date(date);
+        // console.log(releaseDate);
+        return releaseDate.toDateString();
+    }
+
+    return (
+        <ImageList sx={{
+            height:350
+        }} cols={4}>
+            {data.map((item) => (
+                <ImageListItem key={item.id} className='imageItem' >
+                    <img
+                        src={item.poster_url}
+                        alt={item.title}
+                        loading="lazy"
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                        subtitle={`Release Date : ${releaseDate(item.release_date)}`}
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+}
+
+export default ReleasedMovieContent;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import './Home.css'
-import Header from '../../common/header/Header'
-import '../../common/GridList'
-import GridList from '../../common/GridList'
-import moviesData from '../../common/moviesData'
+import React from 'react';
+import './Home.css';
+import Header from '../../common/header/Header';
+import '../../common/GridList';
+import GridList from '../../common/GridList';
+import moviesData from '../../common/moviesData';
+import ReleasedMovieContent from '../../common/ReleasedMovieContent';
 export const Home = () => {
     return (
         <div>
-        <Header heading={'Upcoming Movies'}/>
-        <GridList data={moviesData}/>
-    </div>
+         <Header heading={'Upcoming Movies'} />
+            <GridList data={moviesData} />
+            <div className="flex-container">
+                <div className='left'>
+                    {/* Released Movies Left Side Container */}
+                    <ReleasedMovieContent data={moviesData} />
+                </div>
+                <div className='right'>
+                    {/* Filter Container */}
+                    Filters
+                </div>
+            </div>
+        </div>
+
     )
 }
 
