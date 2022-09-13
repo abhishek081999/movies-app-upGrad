07f88478743ce1b2aed7fa2e50b753ee91f7dca5
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-09-13 15:33:53 +0530

    Checkpoint 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import { Header } from './common/header/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Header />
   </React.StrictMode>
 );
