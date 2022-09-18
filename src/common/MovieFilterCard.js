08289cbaeaeb91d34a9754d12c0012d70700179f
Abhishek Kumar <abhishekbhoranj@gmail.com>
2022-09-18 12:06:13 +0530

    Checkpoint 5

--- src/common/MovieFilterCard.js
+++ src/common/MovieFilterCard.js
@@ -114,6 +114,7 @@ const card = (setFilterObject, filterObject, setMoviesData, moviesData, defaultD
                       }
 
                       if (flag) return movie;
+                      return false;
                   }
                   );
                   setMoviesData(filteredMovies);
